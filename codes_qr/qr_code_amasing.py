c640d91ec9665f7c764465e67dd5e8e060da9a1d
Maria-hub746 <113303447+Maria-hub746@users.noreply.github.com>
2023-11-08 21:50:59 +0200

    Update qr_code_amasing.py

--- codes_qr/qr_code_amasing.py
+++ codes_qr/qr_code_amasing.py
@@ -3,7 +3,6 @@ from main import gen_qr_code
 
 
 text = "https://www.linkedin.com/in/mariia-volochniuk/"
-path_to_download = pictures/img.png #Path().joinpath("pictures", "img.png")  # Путь до фона qr кода
-path_to_save = qr_codes/img.png #Path().joinpath("qr_codes", "img.png")  # Куда сохранять результат и под каким именем (обязательно в png)
-
+path_to_download = Path().joinpath("pictures", "img.png")
+path_to_save = Path().joinpath("qr_codes", "img.png")
 gen_qr_code(text, path_to_download, path_to_save)
